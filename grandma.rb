c53f5c1e9ad4f5131502bc233d392a9e99909232
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-05 02:32:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,13 @@
 # Write a speak_to_grandma method.
 
+def speak_to_grandma
+if phrase == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+elsif phrase == phrase.upcase
+  puts "NO, NOT SINCE 1938!"
+else "HUH?! SPEAK UP SONNY!"
+end
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
